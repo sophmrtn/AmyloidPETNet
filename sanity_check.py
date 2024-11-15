5dc5579c8a5d607d6b797e50054ddbd1845c741b
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-11-15 19:47:57 +0000

    Saves visualisations per slice to disk

--- sanity_check.py
+++ sanity_check.py
@@ -2,12 +2,17 @@ import argparse
 import pandas as pd
 from DeepPET.data import DeepPETDataGenerator
 from monai.visualize.utils import matshow3d
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import shutil
 
 # initialize parser
 parser = argparse.ArgumentParser(description='DeepPET model testing')
 parser.add_argument('data_dir',
                         help='path to save/find extracted and downloaded scans')  
-
+parser.add_argument('--odir', nargs='?',
+                        help='path to odir')  # assumes csv file is within data_dir if unspecified
 parser.add_argument('--metadata', nargs='?',
                         help='path to metadata')  # assumes csv file is within data_dir if unspecified
 parser.add_argument('--ids', nargs='*',
@@ -15,23 +20,39 @@ parser.add_argument('--ids', nargs='*',
 args = parser.parse_args()
 
 # parse arguments 
-ft_df = pd.read_csv(args.metadata)
-# filter by ids
-ft_df = ft_df[ft_df['amypad_id'].isin(args.ids)]
-
-# If label column is a string convert to int
-if type(ft_df['pet_vr_classification'].values[0]) is str:
-    ft_df['pet_vr_classification'] = (ft_df['pet_vr_classification']=="Positive").astype(int)
-
+test_df = pd.read_csv(args.metadata)
+odir = args.odir
+fpaths = [os.path.join(args.data_dir, f) for f in test_df['img_path'].values.flatten()] 
 
 # QC check
 test_gen = DeepPETDataGenerator(
-    args.data_dir,
-    ids=args.ids,
-    labels=ft_df['pet_vr_classification'].values,
+    fpaths=fpaths,
 )
 # preprocess check
 processed_imgs = test_gen.preprocess_for_visualization(test_gen.fpaths)
 
 # visualise img
-matshow3d(processed_imgs[0], frame_dim=-2, show=True, cmap='gray')
+matshow3d(processed_imgs[0], frame_dim=-1, show=True, cmap='gray')
+
+# save img
+img_np = processed_imgs[0]
+
+if os.path.exists(odir):
+    shutil.rmtree(odir)
+os.makedirs(odir, exist_ok=True)
+
+for k in np.arange(img_np.shape[0]):
+    plt.imshow(img_np[k, :, :], cmap="gray")
+    plt.savefig(os.path.join(odir, f"sagittal{k}.png"))
+    plt.close()
+
+for k in np.arange(img_np.shape[1]):
+    plt.imshow(img_np[:, k, :], cmap="gray")
+    plt.savefig(os.path.join(odir, f"coronal_{k}.png"))
+    plt.close()
+
+for k in np.arange(img_np.shape[2]):
+    plt.imshow(img_np[:, :, k], cmap="gray")
+    plt.savefig(os.path.join(odir, f"axial_{k}.png"))
+    plt.close()
+
